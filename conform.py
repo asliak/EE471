3e7015ba600905b8f05a3435a06532dfacc5a3bf
asliak <aksakalaslinur@gmail.com>
2026-03-05 14:40:50 +0300

    Added the pleaseConformOnepass method.

--- conform.py
+++ conform.py
@@ -1,1 +1,9 @@
 #This script was written by Aslı Nur Aksakal.
+def pleaseConformOnepass(caps):
+    caps = caps + [caps[0]]
+    for i in range(1, len(caps)):
+        if caps[i] != caps[i-1]:
+            if caps[i] != caps[0]:
+                print('People in positions', i, end='')
+            else:
+                print(' through', i-1, 'flip your caps!')
